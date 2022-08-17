29aedbf0352c23a234f4c94729895d4a898e7b76
andrejohansenmercado <andrejohansen.mercado.iics@ust.edu.ph>
2022-08-17 13:27:49 +0800

    Layouts

---
+++ Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/AlumniMainMenu.java
@@ -0,0 +1,14 @@
+package com.meyvn.restart_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlumniMainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alumni_main_menu);
+    }
+}
---
+++ Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/SubsCravingQuestionnaire.java
@@ -0,0 +1,14 @@
+package com.meyvn.restart_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubsCravingQuestionnaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.subs_craving_questionnaire);
+    }
+}
