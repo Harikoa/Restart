f8e681d41e0cb4fd184f9d73b2a29cc0083a5d91
Harikoa <edz.manalaotao@gmail.com>
2022-08-21 23:53:32 +0800

    Simple connectivity on firebase database

--- Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/JournalEntry.java
+++ Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/JournalEntry.java
@@ -1,8 +1,27 @@
 package com.meyvn.restart_mobile;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JournalEntry extends AppCompatActivity {
 
@@ -10,5 +29,31 @@ public class JournalEntry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.journal_entry);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        EditText journal = findViewById(R.id.journalEntry);
+        Button submit = findViewById(R.id.journalEntrySubmit);
+        Map <String,Object> map = new HashMap<>();
+        submit.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                map.put("date", new Date().getDate());
+                map.put("Journal Entru",journal.getText().toString());
+            db.collection("Accounts").document("Edwin Manalaotao").collection("Journal").add(map)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error adding document", e);
+                        }
+                    });
+            }
+        });
+
     }
 }
