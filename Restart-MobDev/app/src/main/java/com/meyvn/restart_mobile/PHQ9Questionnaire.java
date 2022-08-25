a7240b5d1d6dcea61ba1057593405e76b619a5f6
DomYuuu <Dominicyu45@gmail.com>
2022-08-25 12:09:20 +0800

    PHQ-9
    Added page + onting elements

---
+++ Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/PHQ9Questionnaire.java
@@ -0,0 +1,14 @@
+package com.meyvn.restart_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PHQ9Questionnaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.phq9_questionnaire);
+    }
+}
