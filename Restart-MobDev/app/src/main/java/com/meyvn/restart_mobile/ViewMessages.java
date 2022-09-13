c45f16db0e15b2726599953f4b58e679d3ae3706
andrejohansenmercado <andrejohansen.mercado.iics@ust.edu.ph>
2022-09-13 15:02:34 +0800

    FCM

--- Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/ViewMessages.java
+++ Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/ViewMessages.java
@@ -1,14 +1,26 @@
 package com.meyvn.restart_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class ViewMessages extends AppCompatActivity {
+
+    EditText Token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_messages);
+
+
     }
 }
