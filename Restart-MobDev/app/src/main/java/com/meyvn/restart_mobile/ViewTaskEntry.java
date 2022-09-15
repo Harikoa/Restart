f1bd511672feafd3c8ef478d20de444b5d94f64f
Harikoa <edz.manalaotao@gmail.com>
2022-09-15 14:47:33 +0800

    View Task Updated

--- Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/ViewTaskEntry.java
+++ Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/ViewTaskEntry.java
@@ -2,7 +2,13 @@ package com.meyvn.restart_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.meyvn.restart_mobile.POJO.ViewTaskPojo;
 
 public class ViewTaskEntry extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class ViewTaskEntry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_task_entry);
+        TextView title = findViewById(R.id.taskEntryTitle);
+        TextView date = findViewById(R.id.taskEntryDate);
+        TextView deadline = findViewById(R.id.taskEntryDeadline);
+        TextView details = findViewById(R.id.taskDetailBody);
+        Intent i = getIntent();
+        Gson convert = new Gson();
+        String JSON = i.getStringExtra("JSON");
+        ViewTaskPojo pojo = convert.fromJson(JSON,ViewTaskPojo.class);
+        title.setText(pojo.getTitle());
+        date.setText("Date Assigned: " +pojo.getTaskDate());
+        deadline.setText("Deadline: " + pojo.getTaskDeadline());
+        details.setText(pojo.getTaskDescription());
+
     }
 }
