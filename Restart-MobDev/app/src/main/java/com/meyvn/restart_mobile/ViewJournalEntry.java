c3345b3b19d87f1759c2222b404f914021882b20
Harikoa <edz.manalaotao@gmail.com>
2022-09-25 14:40:12 +0800

    PushNotif with edit journal

--- Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/ViewJournalEntry.java
+++ Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/ViewJournalEntry.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.meyvn.restart_mobile.POJO.JournalPojo;
+
+import java.time.LocalDate;
 
 public class ViewJournalEntry extends AppCompatActivity {
 
@@ -19,13 +22,13 @@ public class ViewJournalEntry extends AppCompatActivity {
         setContentView(R.layout.view_journal_entry);
         TextView Date = findViewById(R.id.journalEntryDate);
         TextView body = findViewById(R.id.journalEntryBody);
+        Button edit = findViewById(R.id.editJournal);
         Intent i = getIntent();
         String JSON= i.getStringExtra("JSON");
         Gson convert = new Gson();
         JournalPojo pojo = convert.fromJson(JSON,JournalPojo.class);
         Date.setText(pojo.getDate());
         body.setText(pojo.getJournalEntry());
-
         ImageButton back = findViewById(R.id.journalEntryBack);
 
         back.setOnClickListener(new View.OnClickListener() {
@@ -34,7 +37,18 @@ public class ViewJournalEntry extends AppCompatActivity {
                 finish();
             }
         });
+        LocalDate date = LocalDate.parse(pojo.getDate());
+        if(date.equals(LocalDate.now()))
+            edit.setVisibility(View.VISIBLE);
 
-
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),editJournal.class);
+                i.putExtra("JSON",JSON);
+                i.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                startActivity(i);
+            }
+        });
     }
 }
