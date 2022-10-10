54b46ad8bb5dbc58a26ec0bd682b8007f1dd1085
Harikoa <edz.manalaotao@gmail.com>
2022-10-10 17:50:56 +0800

    fix alumni

--- Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/AlumniMainMenu.java
+++ Restart-MobDev/app/src/main/java/com/meyvn/restart_mobile/AlumniMainMenu.java
@@ -13,10 +13,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.gson.Gson;
 import com.meyvn.restart_mobile.POJO.Account;
@@ -24,7 +29,7 @@ import com.meyvn.restart_mobile.POJO.Account;
 import java.util.ArrayList;
 
 public class AlumniMainMenu extends AppCompatActivity {
-
+    FirebaseAuth auth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,11 +41,26 @@ public class AlumniMainMenu extends AppCompatActivity {
         Button messages = findViewById(R.id.alumniMessage);
         SharedPreferences prf = getSharedPreferences("AccountLogged",MODE_PRIVATE);
         SharedPreferences.Editor edit = prf.edit();
+        Button sg = findViewById(R.id.alumniSupportGroup);
+        Button prof = findViewById(R.id.alumniViewProfile);
+        sg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),ViewSupportGroups.class);
+                startActivity(i);
+            }
+        });
+        prof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),ViewProfile.class);
+                startActivity(i);
+            }
+        });
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                edit.remove("Account");
-                edit.apply();
+                auth.signOut();
                 Intent i = new Intent(getApplicationContext(),Login.class);
                 startActivity(i);
                 finish();
@@ -56,30 +76,32 @@ public class AlumniMainMenu extends AppCompatActivity {
     }
     public void updateInfo()
     {
-        Gson gson = new Gson();
         FirebaseFirestore fs = FirebaseFirestore.getInstance();
-        String email = Login.authACC;
+        String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
         FirebaseMessaging.getInstance().getToken()
                 .addOnSuccessListener(new OnSuccessListener<String>() {
                     @Override
                     public void onSuccess(String s) {
                         System.out.println("token"+ s);
-                        fs.collection("Accounts").document(email).update("fcm",s);
-                        fs.collection("Accounts").document(email)
-                                .get()
-                                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        Login.storedAcc.setFCM(s);
+                        Login.storedAcc.setID(UID);
+                        fs.collection("Accounts").document(UID).set(Login.storedAcc, SetOptions.merge())
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
                                     @Override
-                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                        Login.storedAcc = documentSnapshot.toObject(Account.class);
-                                        String JSON = gson.toJson(Login.storedAcc,Account.class);
-                                        SharedPreferences spf = getSharedPreferences("AccountLogged",MODE_PRIVATE);
-                                        SharedPreferences.Editor edit = spf.edit();
-                                        edit.putString("Account",JSON);
-                                        edit.apply();
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful())
+                                        {
+                                            if(!Login.storedAcc.isActivated())
+                                            {
+                                                Toast.makeText(getApplicationContext(),"Account is Deactivated",Toast.LENGTH_LONG).show();
+                                                auth.signOut();
+                                                finish();
+                                            }
+                                        }
                                     }
                                 });
                     }
                 });
-    }
 
+    }
 }
