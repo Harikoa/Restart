a64ce93b0aa9f7f2ea45093ce6d5665148200ced
Harikoa <edz.manalaotao@gmail.com>
2022-10-15 22:29:25 +0800

    Displaying tables

--- RestartWeb ApplicationJS/routes/accountRoutes.js
+++ RestartWeb ApplicationJS/routes/accountRoutes.js
@@ -1,8 +1,27 @@
 const express = require("express")
 const router = express.Router()
 const {addAcc,getAllAcc} = require("../controller/accountController")
+const firebase = require('../config.js')
 
-router.get("/",getAllAcc)
-
+router.get("/",(req,res)=>{
+    if(firebase.auth().currentUser==null)
+    {
+        res.redirect('/')
+    }
+    else
+    {
+    var type= req.query.type
+    var msg =''
+    if(type=='1')
+    msg = "Success!"
+    else if (type=='email')
+    msg = "Email is already in use!"
+    res.render("../htmlFiles/AdminManagePatient",{msg})
+    }
+})
+router.get("/getTable",async (req,res)=>{
+    var role = req.query.role
+    res.json({accounts:await getAllAcc(role)})
+})
 router.post("/create",addAcc)
 module.exports = router
