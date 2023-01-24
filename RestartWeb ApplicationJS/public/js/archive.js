3c906a48e4b1d4d4db3c2319d01aa22af46a633d
DomYuuu <111236761+DomYuuu@users.noreply.github.com>
2023-01-24 18:29:25 +0800

    Arichive scroller and ano

--- RestartWeb ApplicationJS/public/js/archive.js
+++ RestartWeb ApplicationJS/public/js/archive.js
@@ -8,8 +8,8 @@ async function getData()
         var data = await res.json()
         var container = document.querySelector(".attatch")
         data.files.forEach((file)=>{
-            container.insertAdjacentHTML("afterbegin",'<div class="row justify-content-center"><div class="col-sm-12"><div class="formDesign"><h3><b>Name</b>:<a href="/admin/archiveGetFile?id=' + file + '">' + file.substring(0,file.indexOf("-")) + '</a></h3>'+
-            '</div></div></div>')
+            container.insertAdjacentHTML("afterbegin",'<div class="row justify-content-center"><h3 style="text-align: center;"><a href="/admin/archiveGetFile?id=' + file + '">' + file.substring(0,file.indexOf("-")) + '</a></h3>'+
+            '</div></div>')
         })
     })
 }
